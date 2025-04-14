7f6e58a9e08dd5125d57770a1759a564d4aed2dd
Oleg Mysin <o.mysin@crestwavetech.ru>
2025-04-14 17:54:03 +0300

    - lesson 1.2 Tasks in Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,63 @@
+val firstTask = "MyCustomTasks1"
+val secondTask = "MyCustomTasks2"
+val key = "someKey"
+val myGroup = "custom group"
+
+tasks.register(firstTask) {
+    println("$name init!!\uD83E\uDEE0!")
+
+    // doFirst выполняется по типу стека LIFO
+    //doLast выполняются по типу очереди FIFO
+
+    doFirst {
+        println("$name do work 1")
+
+//        project.ext.set(key, "CUSTOM DATA!!!")
+    }
+
+    doFirst {
+        println("$name do work 2")
+    }
+
+    doFirst {
+        println("$name do work 3")
+    }
+
+    doLast {
+        println("$name do last work 1")
+    }
+
+    doLast {
+        println("$name do last work 2")
+    }
+
+    doLast {
+        println("$name do last work 3")
+    }
+}
+
+//tasks.forEach { task ->
+//    if (task.name.contains("Custom")) {
+//        task.group = myGroup
+//    }
+//}
+
+// Будет пройдено по всем таскам, не зависимо от порядка вызова по файлам - живые коллекции
+tasks.configureEach {
+        if (name.contains("Custom")) {
+        group = myGroup
+    }
+}
+
+tasks.register(secondTask) {
+    println("$name init \uD83E\uDD78")
+
+    dependsOn(firstTask)
+
+    doFirst {
+        println("$name do first work")
+
+        println("got data ${project.findProperty(key)}")
+    }
+}
+
